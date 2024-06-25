9a85c05ea4c69df4d22689ed4b82f96346a52e1f
Ronan Avila <avila.ronan@gmail.com>
2024-06-25 07:49:58 -0300

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$green300",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
 export default function Home() {
   return (
     <>
-      <h1>Texto</h1>
+      <Button>
+        <span>aaa</span>Enviar
+      </Button>
     </>
   );
 }
